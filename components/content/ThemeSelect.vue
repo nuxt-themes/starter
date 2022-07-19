e4792232ebec33236a39e3a8687bd9b6d17cb37f
Yaël Guilloux <yael.guilloux@gmail.com>
2022-07-19 15:28:12 +0200

    feat(init): init project (full starter)

---
+++ components/content/ThemeSelect.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const onClick = () => (colorMode.value === 'light' ? (colorMode.preference = 'dark') : (colorMode.preference = 'light'))
+</script>
+
+<template>
+  <button aria-label="Color Mode" class="inline-block w-6" @click="onClick">
+    <ColorScheme placeholder="...">
+      <Icon v-if="colorMode.value === 'dark'" name="heroicons-outline:moon" class="w-6 h-6 dark:text-white" />
+      <Icon v-else name="heroicons-outline:sun" class="w-6 h-6 dark:text-white" />
+    </ColorScheme>
+  </button>
+</template>
