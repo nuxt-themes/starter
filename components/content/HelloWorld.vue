2b890e16a67781ffe89dfb4bea724e07c1ba1dfe
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-08-04 12:50:48 +0200

    refactor: new structure (#3)
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>

--- components/content/HelloWorld.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div>
-  Hello World 👋
-</div>
-</template>
---
+++ theme/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout" :style="{ color: $tokens('colors.primary.200') }">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  /*
+  ** TODO: this does not work
+  color: v-bind($tokens('colors.primary.500'))
+  */
+}
+</style>
